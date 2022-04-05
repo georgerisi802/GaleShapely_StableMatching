e7632873484373ee170951135b63e80d4f122e34
George Risi <george.risi.jr@gmail.com>
2022-04-04 21:47:31 -0400

    forgot a file in last commit

---
+++ src/MatchMaker.java
@@ -0,0 +1,20 @@
+
+public class MatchMaker {
+	
+	// Two arrays representing the preferences of each group
+	
+	private int [][] g1;
+	private int [][] g2;
+	
+	public void Matchmaker (int [][] g1, int [][] g2){
+		this.g1 = g1;
+		this.g2 = g2;
+		
+		
+	}
+	
+	public String dummyResult() {
+		return "TEXT";
+	}
+	
+}
